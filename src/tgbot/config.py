f84020ae62b2cfb9fd00f110dbb6a79388547ef8
mor80 <egmor80@gmail.com>
2025-08-27 00:01:27 +0300

    old main and config

--- src/tgbot/config.py
+++ src/tgbot/config.py
@@ -1,28 +1,23 @@
-# tgbot/config.py
-from functools import lru_cache
+from pathlib import Path
+
+from dotenv import load_dotenv
 from pydantic import AnyHttpUrl, Field
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(
-        extra="ignore",
-        # убрали env_file и env_file_encoding
-    )
-
     bot_token: str = Field(..., env="BOT_TOKEN")
-    api_gateway_url: AnyHttpUrl = Field("http://api-gateway:8080", env="API_GATEWAY_URL")
+
+    api_gateway_url: AnyHttpUrl = Field("http://localhost:8080", env="API_GATEWAY_URL")
     log_level: str = Field("INFO", env="LOG_LEVEL")
 
-    webhook_base: str = Field(..., env=["BOT_WEBHOOK_BASE", "WEBHOOK_BASE"])
-    webhook_path: str = Field("/tg-webhook", env=["BOT_WEBHOOK_PATH", "WEBHOOK_PATH"])
-    # секрет делаем необязательным: можно запускать и без него (для локалки),
-    # а если задан — используем
-    webhook_secret: str = Field("", env=["BOT_WEBHOOK_SECRET"])
-    web_host: str = Field("0.0.0.0", env=["BOT_WEB_HOST", "WEB_HOST"])
-    web_port: int = Field(8081, env=["BOT_WEB_PORT", "WEB_PORT"])
+    # webhook_base: str = Field(..., env=["BOT_WEBHOOK_BASE", "WEBHOOK_BASE"])
+    # webhook_path: str = Field("/tg-webhook", env=["BOT_WEBHOOK_PATH", "WEBHOOK_PATH"])
+    # # секрет делаем необязательным: можно запускать и без него (для локалки),
+    # # а если задан — используем
+    # webhook_secret: str = Field("", env=["BOT_WEBHOOK_SECRET"])
+    # web_host: str = Field("0.0.0.0", env=["BOT_WEB_HOST", "WEB_HOST"])
+    # web_port: int = Field(8081, env=["BOT_WEB_PORT", "WEB_PORT"])
 
 
-@lru_cache
-def get_settings() -> Settings:
-    return Settings()
+settings = Settings()  # singleton
