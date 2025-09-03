23b09f6b8e01a91f5576d6dcf1dc9d5bb5331619
mor80 <egmor80@gmail.com>
2025-09-03 19:40:23 +0300

    temporary test 2

--- src/tgbot/handlers/menu/pay_sections.py
+++ src/tgbot/handlers/menu/pay_sections.py
@@ -1,8 +1,5 @@
-import asyncio
-import contextlib
 import logging
 from aiogram import Router, F
-from aiogram.exceptions import TelegramBadRequest
 from aiogram.types import CallbackQuery
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
@@ -240,15 +237,6 @@ async def cb_paysec_open_subject(cb: CallbackQuery, api_client: APIGatewayClient
     if len(locked) == 0:
         # t("pay_subject_all_bought").format(subject=subject)
         await cb.answer(t("pay_sections_empty"), show_alert=True)
-
-        async def _later():
-            await asyncio.sleep(0.5)
-            page = subj_idx // PER_PAGE_SUBJ
-            with contextlib.suppress(TelegramBadRequest):
-                await render_paysec_subjects(cb.message, cb.from_user.id, api_client, page=page, state=state)
-
-        asyncio.create_task(_later())
-        return
 
     await cb.answer()
     await render_paysec_sections(cb.message, cb.from_user.id, api_client, subj_idx=subj_idx, page=0, state=state)
